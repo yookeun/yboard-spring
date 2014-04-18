a79a64ec48e237e52864e4b94c2d5b5d6444a4db
yookeun <ykkim@andwise.com>
2014-04-18 15:42:15 +0900

    First Commit

---
+++ src/main/java/Ognl.java
@@ -0,0 +1,158 @@
+import java.lang.reflect.Array;
+import java.util.Collection;
+import java.util.Map;
+
+
+/**
+ * utils class for Ibatis 
+ * 
+ * example:
+ * <pre>
+ * 	&lt;if test="@Ognl@isNotEmpty(userId)">
+ *		and user_id = #{userId}
+ *	&lt;/if>
+ * </pre>
+ * @author badqiu
+ *
+ */
+
+
+/**
+ * mybatis의 각  xml 파일에서 사용하는 클래스이다. 현재 디폴트 패키지를 그대로 유지할 것!!! 
+ * 소스출처  : http://code.google.com/p/mybatis/issues/detail?id=210
+ * @author ykkim
+ *
+ */
+public  class Ognl {
+	
+	/**
+	 * test for Map,Collection,String,Array isEmpty
+	 * @param o
+	 * @return
+	 */
+	@SuppressWarnings("rawtypes")
+	public static boolean isEmpty(Object o) throws IllegalArgumentException {
+		
+		if(o == null) return true;
+
+		if(o instanceof String) {
+			if(((String)o).length() == 0){
+				return true;
+			}
+		} else if(o instanceof Collection) {
+			if(((Collection)o).isEmpty()){
+				return true;
+			}
+		} else if(o.getClass().isArray()) {
+			if(Array.getLength(o) == 0){
+				return true;
+			}
+		} else if(o instanceof Map) {
+			if(((Map)o).isEmpty()){
+				return true;
+			}
+		}else {
+			return false;
+//			throw new IllegalArgumentException("Illegal argument type,must be : Map,Collection,Array,String. but was:"+o.getClass());
+		}
+
+		return false;
+	}
+	
+	/**
+	 * test for Map,Collection,String,Array isNotEmpty
+	 * @param c
+	 * @return
+	 */	
+	public static boolean isNotEmpty(Object o) {
+		return !isEmpty(o);
+	}
+	
+	public static boolean isNotBlank(Object o) {
+		return !isBlank(o);
+	}
+	
+	public static boolean isNumber(Object o) {
+		if(o == null) return false;
+		if(o instanceof Number) {
+			return true;
+		}
+		if(o instanceof String) {
+			String str = (String)o;
+			if(str.length() == 0) return false;
+			if(str.trim().length() == 0) return false;
+			return org.apache.commons.lang.StringUtils.isNumeric(str);
+		}
+		return false;
+	}
+	
+	public static boolean isBlank(Object o) {
+		if(o == null)
+			return true;
+		if(o instanceof String) {
+			String str = (String)o;
+			return isBlank(str);
+		}
+		return false;
+	}
+
+	public static boolean isBlank(String str) {
+		if(str == null || str.length() == 0) {
+			return true;
+		}
+		
+		for (int i = 0; i < str.length(); i++) {
+			if (!Character.isWhitespace(str.charAt(i))) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	/**
+	 * 주어진 문자열이 존재하는 체크 
+	 * @author ykkim 
+	 * @param o
+	 * @param str
+	 * @return
+	 */
+	public static boolean isEqual(Object o, String str){
+		if(o == null) return false;
+		if(o instanceof String) {
+			if(((String)o).equals(str)){			
+				return true;
+			}else{
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	/**
+	 * 주어진 문자열이 존재하지 않는 지  체크 
+	 * @author ykkim 
+	 * @param o
+	 * @param str
+	 * @return
+	 */
+	public static boolean isNotEqual(Object o, String str){		
+		return !isEqual(o,str);
+	}
+	
+	/**
+	 * 주어진 값이 숫자 0이 아닌지 체크 
+	 * @param o
+	 * @return
+	 */
+	public static boolean isNotZero(Object o) {
+		if(!isNumber(o)) {
+			return false;
+		} 		
+		if(Integer.parseInt(String.valueOf(o)) == 0) {			
+			return false;
+		}			
+		return true;
+	}
+	
+
+}
